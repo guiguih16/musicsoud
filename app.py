d92534fc01daeb9354d546de59f1b1645ff3df2f
guiguih16 <projetoimperiodigital@gmail.com>
2026-03-01 20:55:10 -0300

    Update app.py

--- app.py
+++ app.py
@@ -8,8 +8,8 @@ from flask import Flask, render_template, request, redirect, url_for, session, j
 # Conexão com MongoDB
 # -------------------------------
 
-# mongo_uri = os.environ.get("MONGO_URL") >> USAR NO RAILWAY    
-mongo_uri = "mongodb://localhost:27017"
+mongo_uri = os.environ.get("MONGO_URL") >> USAR NO RAILWAY    
+# mongo_uri = "mongodb://localhost:27017"
 if mongo_uri is None:
     raise Exception("MONGO_URL não encontrada no Railway")
 
@@ -271,3 +271,4 @@ def logout():
 # -------------------------------
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=8080)
+
